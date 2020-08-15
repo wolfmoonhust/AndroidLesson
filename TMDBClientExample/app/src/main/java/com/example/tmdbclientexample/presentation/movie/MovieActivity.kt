88cd028edd0c035ee6688b11e0833861aa82b7b9
wolfmoonhust <wolfmoon.hust@gmail.com>
2020-08-15 23:24:44 +0700

    add recyclerView and MovieAdapter for MovieActivity

--- TMDBClientExample/app/src/main/java/com/example/tmdbclientexample/presentation/movie/MovieActivity.kt
+++ TMDBClientExample/app/src/main/java/com/example/tmdbclientexample/presentation/movie/MovieActivity.kt
@@ -2,10 +2,13 @@ package com.example.tmdbclientexample.presentation.movie
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tmdbclientexample.R
 import com.example.tmdbclientexample.databinding.ActivityMovieBinding
 import com.example.tmdbclientexample.presentation.di.Injector
@@ -16,6 +19,7 @@ class MovieActivity : AppCompatActivity() {
     lateinit var factory: MovieViewModelFactory
     private lateinit var movieViewModel: MovieViewModel
     private lateinit var binding: ActivityMovieBinding
+    private lateinit var adapter: MovieAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_movie)
@@ -24,10 +28,31 @@ class MovieActivity : AppCompatActivity() {
 
         movieViewModel = ViewModelProvider(this, factory).get(MovieViewModel::class.java)
 
+        initRecycleView()
+
+
+    }
+
+    private fun initRecycleView(){
+        binding.movieRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter()
+        binding.movieRecyclerView.adapter =  adapter
+
+        dispatchPopularMovies()
+    }
+
+    private fun dispatchPopularMovies() {
+        binding.movieProgressBar.visibility = View.VISIBLE
         val responseLiveData = movieViewModel.getMovies()
         responseLiveData.observe(this, Observer {
-            Log.i("MYTAG", it.toString())
+            if ( it != null) {
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            } else {
+                binding.movieProgressBar.visibility = View.GONE
+                Toast.makeText(applicationContext, "No data available", Toast.LENGTH_LONG).show()
+            }
         })
-
     }
 }
